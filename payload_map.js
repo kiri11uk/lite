39f33cabfb1d1c42d8e284d457ea34ff69a32ec4
Mouuu㋡ <119304810+ps5xploit@users.noreply.github.com>
2024-08-07 14:27:18 +0200

    Update payload_map.js

--- payload_map.js
+++ payload_map.js
@@ -3,7 +3,7 @@ const payload_map =
         {
             displayTitle: 'etaHEN',
             description: 'FPKG enabler ',
-            fileName: 'etaHEN-1.7B.bin',
+            fileName: 'etaHEN-1.7B.bin.gz',
             author: 'LightningMods_, sleirsgoevy, ChendoChap, astrelsky, illusion',
             source: 'https://github.com/LightningMods/etaHEN',
             version: '1.6 beta'
