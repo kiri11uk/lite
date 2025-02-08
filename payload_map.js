f3c0dde4e6f03bcf7f05ca2783bf9459010f66ff
Mouuu㋡ <119304810+ps5xploit@users.noreply.github.com>
2025-02-08 14:30:45 +0100

    Add files via upload

--- payload_map.js
+++ payload_map.js
@@ -1,14 +1,12 @@
 const payload_map = [
-   {
-        displayTitle: 'Hello-XPLOIT',
-        description: 'Hello-XPLOITr ',
-        fileName: 'hello_world.elf',
-        author: 'm0ur0ne',
-        source: 'ps5xploit',
-        version: '1.00'
+    {
+        displayTitle: 'ps5-debug',
+        description: 'ps5-debug ',
+        fileName: '1-ps5debug_10b2.elf',
+        author: 'goldhen',
+        source: 'n',
+        version: '1.02'
     },
-
- 
     {
         displayTitle: 'etaHEN',
         description: 'FPKG enabler ',
@@ -17,21 +15,5 @@ const payload_map = [
         source: 'https://github.com/LightningMods/etaHEN',
         version: '1.8 beta'
     },
-{
-        displayTitle: 'ps5-debug',
-        description: 'ps5-debug ',
-        fileName: '1-ps5debug_10b2.elf',
-        author: 'goldhen',
-        source: 'n',
-        version: '1.02'
-    },
-    // Otros payloads que no cargaran auto.
-{
-        displayTitle: 'elfldr',
-        description: 'elfldr ',
-        fileName: 'elfldr.elf',
-        author: 'john',
-        source: 'n',
-        version: '1.00'
-    },
+    // Otros payloads si es necesario
 ];
